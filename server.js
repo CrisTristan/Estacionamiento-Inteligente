75542ed9bf3648c85bd4ffcc6a2619101f91b5c8
Cristian <cristian.abad700@gmail.com>
2026-05-18 11:38:19 -0500

    se creo el endpoint para creacion de alumno y generacion de QR

--- server.js
+++ server.js
@@ -31,6 +31,54 @@ app.get('/api/alumnos', (req, res) => {
   }); 
 }); 
 
+app.post('/api/alumnos', async (req, res) => { 
+    // Endpoint para crear un nuevo alumno con validación y generación de código QR único
+  try { 
+
+    const { matricula, nombre, auto_placa, activo } = req.body; 
+    const errores = validarDatosAlumno(matricula, nombre, auto_placa || ''); 
+    if (errores.length > 0) { 
+      return res.status(400).json({ 
+        error: errores.join(' ') 
+      }); 
+    } 
+    if (!matricula || !nombre) { 
+      return res.status(400).json({ 
+        error: 'Matrícula y nombre son obligatorios.' 
+      }); 
+    }
+    const qr_code = generarCodigoQRSeguro(); 
+    const qrFileName = generarNombreArchivoQR(); 
+    const qrPath = path.join(qrDir, qrFileName); 
+    //Aqui se guarda el archivo QR en formato PNG en el sistema de archivos, asegurando que cada código sea único y evitando colisiones
+    await QRCode.toFile(qrPath, qr_code, { 
+      width: 300, 
+      margin: 2 
+    }); 
+    // Insertar nuevo alumno en la base de datos con el código QR generado
+    db.run( 
+      `INSERT INTO alumnos (matricula, nombre, qr_code, qr_file, auto_placa, activo) 
+       VALUES (?, ?, ?, ?, ?, ?)`, 
+      [matricula, nombre, qr_code, qrFileName, auto_placa || '', activo ? 1 : 0], function (err) { 
+        if (err) { 
+          return res.status(400).json({ 
+            error: 'No se pudo registrar el alumno. Verifica que la matrícula no esté repetida.' 
+          }); 
+        } 
+        res.json({ 
+          ok: true, 
+          id: this.lastID, 
+          qr_url: `/qrs/${qrFileName}`, 
+          mensaje: `Alumno ${nombre} registrado correctamente.` 
+        }); 
+      } 
+    ); 
+
+    }catch (error) {
+        res.status(500).json({ error: 'Error al crear alumno.' }); 
+    }
+});
+
 app.listen(PORT, () => { 
   console.log(`Servidor corriendo en http://localhost:${PORT}`); 
 });
